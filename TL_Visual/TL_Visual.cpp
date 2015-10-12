e6fe4d8585bd4ffab979ed98c81514dfe57b1407
Daniel Reynolds <cybers_25@hotmail.co.uk>
2015-10-12 12:52:06 +0100

    TL-Engine Commit

---
+++ TL_Visual/TL_Visual.cpp
@@ -0,0 +1,31 @@
+// TL_Visual.cpp: A program using the TL-Engine
+
+#include <TL-Engine.h>	// TL-Engine include file and namespace
+using namespace tle;
+
+void main()
+{
+	// Create a 3D engine (using TLX engine here) and open a window for it
+	I3DEngine* gameEngine = New3DEngine( kTLX );
+	gameEngine->StartWindowed();
+
+	// Add default folder for meshes and other media
+	gameEngine->AddMediaFolder( "C:\\ProgramData\\TL-Engine\\Media" );
+	gameEngine->AddMediaFolder(".\\Media");
+
+	/**** Set up your scene here ****/
+
+
+	// The main game loop, repeat until engine is stopped
+	while (gameEngine->IsRunning())
+	{
+		// Draw the scene
+		gameEngine->DrawScene();
+
+		/**** Update your scene each frame here ****/
+
+	}
+
+	// Delete the 3D engine now we are finished with it
+	gameEngine->Delete();
+}
