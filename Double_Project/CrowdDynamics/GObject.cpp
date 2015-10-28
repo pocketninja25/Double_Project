65252c01d4dd1e2868a317d3e7ba403b325b9c50
Daniel Reynolds <cybers_25@hotmail.co.uk>
2015-10-28 14:00:01 +0000

    Added some functions/data members

--- Double_Project/CrowdDynamics/GObject.cpp
+++ Double_Project/CrowdDynamics/GObject.cpp
@@ -4,9 +4,21 @@
 
 GObject::GObject()
 {
+	UID = 0;
 }
 
 
 GObject::~GObject()
 {
+}
+
+
+void GObject::Update(float frameTime)
+{
+}
+
+
+int GObject::GetUID()
+{
+	return 0;
 }
--- Double_Project/CrowdDynamics/GObject.h
+++ Double_Project/CrowdDynamics/GObject.h
@@ -4,5 +4,10 @@ class GObject
 public:
 	GObject();
 	virtual ~GObject();
+private:
+	int UID;
+public:
+	virtual void Update(float timeStep);
+	int GetUID();
 };
 
